d30f0ca1ce21c35775a25d511ed0dbcb16a8365b
CoolsDayness <54150966+CoolsDayness@users.noreply.github.com>
2020-03-09 19:44:48 -0400

    d

--- src/main/java/frc/robot/commands/PneumaticUp.java
+++ src/main/java/frc/robot/commands/PneumaticUp.java
@@ -7,7 +7,6 @@
 
 package frc.robot.commands;
 
-import java.lang.module.ModuleDescriptor.Requires;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.PneumaticsSubsystem;
